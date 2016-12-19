68881d66c5e42c3070018da02239efdb769b3e48
Graham Hukill <ghukill@gmail.com>
2016-12-19 19:28:18 +0000

    updates

--- wsudorauth/settings.py
+++ wsudorauth/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'wsudorauth.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'wsudorauth',
+        'USER': 'root',
+        'PASSWORD': 'mybadpassword',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
