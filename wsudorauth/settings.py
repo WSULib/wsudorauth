d34e50b906a14cc0056d1d7126444f69d3f93b6f
Graham Hukill <ghukill@gmail.com>
2016-12-20 17:42:12 +0000

    modifications

--- wsudorauth/settings.py
+++ wsudorauth/settings.py
@@ -85,15 +85,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql', 
-    #     'NAME': 'wsudorauth',
-    #     'USER': 'root',
-    #     'PASSWORD': 'mybadpassword',
-    #     'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-    #     'PORT': '3306',
-    # }
+    }
 }
 
 
